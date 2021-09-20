9fea59c990c9b643056c9c3e0ff4761b910b8b05
Cipher Jana <71997730+Arceus-sj@users.noreply.github.com>
2021-09-21 00:01:57 +0530

    javascript  variable implementation

---
+++ variable.js
@@ -0,0 +1,22 @@
+// There are three types of variable:- var, let, const
+
+
+let count = 100;//let cannot be Redeclared
+
+console.log(count)
+
+
+var myAge = 20;
+
+console.log(myAge);
+
+var a; // undefined
+let b = true; //boolean
+const c = 'raja'; //string or character
+var s = "Cipher";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(s);
+ 
