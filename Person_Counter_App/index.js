d1789f1cd49d0d2a7e23828c30b78dd6e5f3117a
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2021-10-01 00:00:20 +0530

    Update index.js

--- Person_Counter_App/index.js
+++ Person_Counter_App/index.js
@@ -1,16 +1,26 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-txt");
 let count = 0;
 
-console.log(countEl);
+
 
 function increment() {
     console.log("Button is clicked..."); 
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    // countEl.innerText = count;
+    countEl.textContent = count;
+    // console.log(count);
 }
+
+
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    // saveEl.innerText += countStr;
+    // console.log(countStr)
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
 
-save();
+ 
