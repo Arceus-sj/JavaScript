a9f8598abb25777f4424a37d0939e2b3e847cd9e
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2021-10-05 23:50:26 +0530

    Create index.js

---
+++ JsBasics/onclick_color_btn/index.js
@@ -0,0 +1,14 @@
+function change_color_1() { 
+  document.getElementById("text").style.color = "red";
+  console.log("color changed to red!!");
+}
+
+function change_color_2() { 
+  document.getElementById("text").style.color = "blue";
+  console.log("color changed to blue!!");
+}
+
+function change_color_3() { 
+  document.getElementById("text").style.color = "yellow";
+  console.log("color changed to yellow!!");
+}
