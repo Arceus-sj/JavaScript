151ea82348d73008afb06c6d80da8f0eea383013
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2021-10-08 23:34:22 +0530

    Create dataType.js

---
+++ JsBasics/Variable_And_DataType/dataType.js
@@ -0,0 +1,42 @@
+
+
+let a = true;
+let b = 50.555216341654546565465654654654;
+let c = "Cipher";
+let d;
+let e = null;
+
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+/*
+At a very high level, There are two types of data types: 
+1. Primitive data types --> undefined, null, number, string, boolean, symbol.
+2. Reference data types --> arrays and object.
+*/
+
+//objects (in from of key-value pairs)
+var info = {
+    firstName: "Cipher",
+    lastName: "Jana",
+    age: 20
+}
+
+console.log(info);
+console.log(typeof(info));
+
+
+
+// array data type
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr);
+console.log(typeof(arr));
+
+console.log(arr[0]);
+console.log(arr[4]);
+console.log(arr[5]);
