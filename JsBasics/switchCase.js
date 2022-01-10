5c982d3623637a395894d82eb4e88b972d72167f
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-01-10 21:53:23 +0530

    Switch Case In JS

---
+++ JsBasics/switchCase.js
@@ -0,0 +1,24 @@
+let user = "subadmin";
+
+switch (user) {
+    case "admin":
+        console.log("You get full access");
+        break;
+    
+    case "subadmin":
+        console.log("You get access to create/delete courses");
+        break;
+    
+    case "testprep":
+        console.log("You get access to create/delete tests");
+        break;
+    
+    case "user":
+        console.log("You get access to consume content");
+        break;
+    
+    default:
+        console.log("Trial user");
+        break;
+}
+
