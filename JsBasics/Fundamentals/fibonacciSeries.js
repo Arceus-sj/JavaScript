18b780210751db6843f7935507f7acf33bec8ada
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-04-18 00:09:20 +0530

    Fibonacci Code challenge by Angela Yu

--- JsBasics/Fundamentals/fibonacciSeries.js
+++ JsBasics/Fundamentals/fibonacciSeries.js
@@ -1,12 +1,20 @@
 function fibonacciGenerator(n) {
   let ans = [0,1];
-  for(let i = 0, startNum = 0, endNum = 1; i < n; i++) {
-    let sum = startNum + endNum;
-    ans.push(sum);
-    startNum = endNum;
-    endNum = sum;
-  }
   
+  if(n === 1) {
+    return '0';
+  }
+  else if (n === 2) {
+    return ans;
+  }
+  else {
+    for(let i = 2, startNum = 0, endNum = 1; i < n; i++) {
+      let sum = startNum + endNum;
+      ans.push(sum);
+      startNum = endNum;
+      endNum = sum;
+    }  
+  }
   return ans;
 }
 
@@ -15,4 +23,4 @@ let numberOfItems = 10;
 
 
 
-console.log(fibonacciGenerator(numberOfItems));
+console.log(fibonacciGenerator(10));
