f34eb0d67343a7c52844181eca71538bae23aac7
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-05-19 23:47:41 +0530

    Create objectAccess.js

---
+++ JsBasics/ObjectsInJS/objectAccess.js
@@ -0,0 +1,12 @@
+const bellBoy = {
+  name: 'Ash',
+  age: 25,
+  hasWorkPermit: true,
+  languages: ["English", "Japanese", "French"]
+};
+
+console.log(bellBoy);
+
+console.log(bellBoy.name);
+
+console.log(bellBoy['hasWorkPermit']);
