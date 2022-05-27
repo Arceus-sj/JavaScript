2175a87325766ca01ff4397d2e5aae5e66062660
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-05-27 23:11:36 +0530

    Create accessObjectFromArray.js

---
+++ JsBasics/ObjectsInJS/accessObjectFromArray.js
@@ -0,0 +1,42 @@
+
+let data = [
+    {
+        id: 1,
+        name:  "Yaroslava Borz",
+        job:  "WEB DEVELOPER",
+        img: "./asset/person_1.jpg", 
+        info: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        id: 2,
+        name: "Bill Anderson",
+        job: "THE BOSS",
+        img: "./asset/person_2.jpg", 
+        info: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    },
+    {
+        id: 3,
+        name: "Peter Jones", 
+        job: "INTERN",
+        img: "./asset/person_3.jpg",  
+        info: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+    {
+        id: 4,
+        name: "Susan Smith", 
+        job: "WEB DESIGNER",
+        img:"./asset/person_4.jpg",  
+        info: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+        id: 5,
+        name: "Anna Johnson", 
+        job: "Photographer",
+        img: "./asset/person_5.jpg",  
+        info: "Super responsive, quality work, wholesome professionals. You are best at what you all do. One of my best wedding decisions i have ever taken. Anna , Thank You from the bottom of my heart for making our day even more special and memorable."
+    }
+];
+
+for(let i = 0; i < 5; i++) {
+  console.log(data[i].name);
+}
