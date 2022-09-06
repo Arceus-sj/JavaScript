e2bb0d07d449daa3b1fefe4526c0dc7f71ceb5af
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-09-06 19:57:53 +0530

    NPM superheroes and supervillains project

---
+++ superHeroName/index.js
@@ -0,0 +1,8 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+
+superheroes.all;
+supervillains.all;
+
+console.log(`Super Hero --> ${superheroes.random()} V/S Super Villain --> ${supervillains.random()}`);
