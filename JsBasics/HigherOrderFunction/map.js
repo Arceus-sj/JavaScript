af122733c10d89e5e0d0bbce43be94140c4883ce
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2023-01-09 17:49:36 +0530

    Update map.js

--- JsBasics/HigherOrderFunction/map.js
+++ JsBasics/HigherOrderFunction/map.js
@@ -1,7 +1,17 @@
 //  map() : loops and return an array
 
-const doubleArray = (number) => {
-  return number.map(number => number * 2);
-}
+// const doubleArray = (number) => {
+//   return number.map(number => number * 2);
+// }
 
-doubleArray([1,2,3,4,5]).map(number => console.log(number));
+// doubleArray([1,2,3,4,5]).map(number => console.log(number));
+
+// map: create a new array by performing some operation on each array element
+
+let arr = [40,34,69];
+
+let a = arr.map((value) => {
+  return value + 1;
+})
+
+console.log(a);
